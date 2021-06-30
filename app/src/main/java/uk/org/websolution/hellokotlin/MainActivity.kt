84062d8443f7d300ff3baafd1a8d7dd48ed5a127
Alex <alexdolzenko@gmail.com>
2021-06-30 21:55:34 +0100

    Перевёл main-activity в котлин.

--- app/src/main/java/uk/org/websolution/hellokotlin/MainActivity.kt
+++ app/src/main/java/uk/org/websolution/hellokotlin/MainActivity.kt
@@ -1,11 +1,18 @@
 package uk.org.websolution.hellokotlin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var startButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startButton = findViewById(R.id.start_button)
+        startButton?.setOnClickListener{
+            Toast.makeText(this, startButton?.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
